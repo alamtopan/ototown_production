f57e42217e31f2552d8df8bf1ce438ea0da07789
alamtopan <alamtopani@yahoo.com>
2014-04-15 18:46:07 -0400

    push

--- db/migrate/20140410090740_change_active_string_to_boolean.rb
+++ db/migrate/20140410090740_change_active_string_to_boolean.rb
@@ -2,8 +2,4 @@ class ChangeActiveStringToBoolean < ActiveRecord::Migration
   def up
    change_column :advertisements, :active, :boolean
   end
-
-  def down
-   change_column :advertisements, :active, :string
-  end
 end
