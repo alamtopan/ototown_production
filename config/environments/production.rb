47c0bb8513e36b138b6962f85bcd182f24b2b415
alamtopan <alamtopani@yahoo.com>
2014-04-22 10:34:24 -0400

    add smpt gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,16 @@ Ototown::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.asset_host = 'http://ototown.com'
+  config.action_mailer.default_url_options = { host: 'ototown.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => 'dfedogawa3',
+    :password             => 'reyhan edogawa',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  
+  }
+
 end
