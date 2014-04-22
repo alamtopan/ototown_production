b9a6e1d88c233e23b89dc0f47da5b238c6c93dcc
alamtopan <alamtopani@yahoo.com>
2014-04-22 13:17:40 -0400

    update skip confirmation

--- app/models/operator.rb
+++ app/models/operator.rb
@@ -8,18 +8,13 @@ class Operator < ActiveRecord::Base
 
   class << self
     def find_for_facebook_oauth(auth)
-    	unless user = where({provider: auth.provider, uid: auth.uid, email: auth.info.email}).first
-	      user = Operator.new
-	      user.provider = auth.provider
-	      user.uid = auth.uid
-	      user.email = auth.info.email
-	      user.password = Devise.friendly_token[0,20]
-	      user.username = auth.info.nickname
-        user.skip_confirmation!
-        user.save
-        user
-	    end
-	    user
+      where(auth.slice(:provider, :uid)).first_or_create do |user|
+          user.provider = auth.provider
+          user.uid = auth.uid
+          user.email = auth.info.email
+          user.password = Devise.friendly_token[0,20]
+          user.username = auth.info.nickname
+      end
     end
 
     def new_with_session(params, session)
