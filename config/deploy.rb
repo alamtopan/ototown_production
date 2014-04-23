7050d9a3b5ce163f9b05c7a5b3b6635e9e95855f
alamtopan <alamtopani@yahoo.com>
2014-04-23 09:04:55 -0400

    change deployment script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,13 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'ototown'
+set :application, 'Ototown'
 set :repo_url, 'git@github.com:alamtopan/ototown_production.git'
+set :scm, :git
 set :deploy_to, '/home/ototown/www'
+
+set :format, :pretty
+set :log_level, :info
 
 set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp vendor/bundle public/system}
@@ -21,26 +25,17 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
-      execute "cd #{current_path} && bundle exec unicorn -c #{current_path}/config/unicorn.rb -E production -D"
-    end
-  end
-
-  task :stop do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute "ln -nfs #{shared_path}/tmp #{release_path}/tmp"
-      execute "ln -nfs #{shared_path}/log #{release_path}/log"
       execute "kill -15 `cat #{current_path}/tmp/pids/unicorn.pid` || true"
+      execute "cd #{current_path} && bundle exec unicorn -c #{current_path}/config/unicorn.rb -E production -D"
     end
   end
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      stop
-      start
       # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      within release_path do
+        execute :rake, 'cache:clear'
+      end
     end
   end
 
