84539fa042d5e43c9c9f55bedc58d0e03981c78f
alamtopan <alamtopani@yahoo.com>
2014-04-23 09:33:24 -0400

    update deployment script

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,21 +23,11 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-      execute "kill -15 `cat #{current_path}/tmp/pids/unicorn.pid` || true"
-      execute "cd #{current_path} && bundle exec unicorn -c #{current_path}/config/unicorn.rb -E production -D"
-    end
-  end
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      within release_path do
-        execute :rake, 'cache:clear'
-      end
+      invoke 'unicorn:restart'
     end
   end
 
   after :finishing, 'deploy:cleanup'
 end
+
+after 'deploy:publishing', 'deploy:restart'
