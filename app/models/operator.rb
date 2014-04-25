3170dee72808cb5a81c73dc052d1013c4cfe263f
alamtopan <alamtopani@yahoo.com>
2014-04-25 05:22:58 -0400

    push revision

--- app/models/operator.rb
+++ app/models/operator.rb
@@ -6,30 +6,6 @@ class Operator < ActiveRecord::Base
   devise :database_authenticatable, :lockable, :timeoutable, 
          :recoverable, :rememberable, :trackable, :validatable
 
-
-  class << self
-    def find_for_facebook_oauth(auth)
-      where(auth.slice(:provider, :uid)).first_or_create do |user|
-          user.provider = auth.provider
-          user.uid = auth.uid
-          user.email = auth.info.email
-          user.password = Devise.friendly_token[0,20]
-          user.username = auth.info.nickname
-          user.confirmation_sent_at = Time.now - 5.minutes
-          user.confirmed_at = Time.now
-      end
-    end
-
-    def new_with_session(params, session)
-      super.tap do |user|
-        if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-          user.email = data["email"] if user.email.blank?
-        end
-      end
-    end
-
-  end
-
   def not_dealer?
     self.dealer_info.active.nil?
   end
