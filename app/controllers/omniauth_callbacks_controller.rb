d1167d346a2ac8b79bdde387c00a558574578c28
deviff <dfedogawa3@gmail.com>
2014-05-01 12:53:36 +0700

    try to fix facebook login

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,7 +1,10 @@
-class OmniauthCallbacksController < Devise::OmniauthCallbacksController   
-	def facebook     
-    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)      
-    if @user.persisted?       
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+	def facebook
+    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
+    if @user.persisted?
+      @user.confirmation_token = nil
+      @user.confirmed_at = Time.now
+      @user.save
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
