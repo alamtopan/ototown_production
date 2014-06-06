e292a4f0bb34ea8314422f167f014890d2e742b7
alamtopan <alamtopani@yahoo.com>
2014-06-06 14:08:46 +0700

    revision

--- db/migrate/20140605164721_change_column_price_string_to_integer.rb
+++ db/migrate/20140605164721_change_column_price_string_to_integer.rb
@@ -1,9 +1,5 @@
 class ChangeColumnPriceStringToInteger < ActiveRecord::Migration
-  def up
-    change_column :products, :price,  :integer, limit: 11
-  end
-
-  def down
-    change_column :products, :price,  :string
+  def change
+    change_column :products, :price,  :integer
   end
 end
