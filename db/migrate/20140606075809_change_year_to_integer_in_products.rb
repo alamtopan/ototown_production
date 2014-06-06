1abf7c8baf023a7e5019c8f3b79ced5eaaa47030
alamtopan <alamtopani@yahoo.com>
2014-06-06 11:48:29 +0700

    change

--- db/migrate/20140606075809_change_year_to_integer_in_products.rb
+++ db/migrate/20140606075809_change_year_to_integer_in_products.rb
@@ -1,5 +1,6 @@
 class ChangeYearToIntegerInProducts < ActiveRecord::Migration
   def change
   	change_column :products, :year, :integer
+  	#change_column :products, :year, 'integer USING CAST(column_name AS integer)'
   end
 end
